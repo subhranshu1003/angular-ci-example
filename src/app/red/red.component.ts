c9a40b4a2c02cbce51d772680fde6387112dbfaa
Fredrik Lundin <fredrik.lundin@capgemini.com>
2017-03-28 21:30:14 +0200

    build(ci-test): testing the CI trigger

--- src/app/red/red.component.ts
+++ src/app/red/red.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-red',
   template: `
-    <div class="wrapper"></div>
+    <div class="wrapper"> </div>
   `,
   styles: ['div.wrapper { width: 500px; height: 500px; background-color: red; }']
 })
